4e94acb9556c248ecbaa55e4630ca3cb8e5a9d41
Kristopher De Los Santos <Kristophersdelossantons@gmail.com>
2020-05-13 14:53:00 -0600

    Got the table to display all the information properly. In the 'app.py' file I used the 'round' tool in order to round the numbers up so they don't give me 8 decimal places. This may lead to innacuracies with money but it's not really a concern. It looks nice and it performs the primary function which was the goal all along

--- app.py
+++ app.py
@@ -26,10 +26,28 @@ def form_fill():
 def results():
     tips_to_split = request.form.get("tip_pool")
     hours_worked = request.form.getlist("number_of_hours")
+    hour_total = 0
+    
+    ind_tips = []
+    #Here i need to do math
+    # Pool = (Tips/Total hours of all servers) * Hour per server
+    # Loop through the hours to add them together to get the total num of hours
+    # Divide the tip pool by that
+    # Multipy that value by each hour to get the amount received for each server
+    for emp in hours_worked:
+        hour_total += int(emp)
+    
+    tip_division = int(tips_to_split)/hour_total
+
+    for emp in hours_worked:
+        ind_tips.append(round(tip_division * int(emp)))
+    
     print(hours_worked)
 
+    hours_tips = zip(hours_worked,ind_tips)
 
-    return render_template("results.html",tips_to_split = tips_to_split, hours_worked = hours_worked )
+
+    return render_template("results.html",tips_to_split = tips_to_split, hours_worked = hours_worked, hour_total = hour_total, tip_division = tip_division, ind_tips = ind_tips, hours_tips = hours_tips )
 
 
 
